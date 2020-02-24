a74a615b9fdd609e499dade9b36ab086425cae78
Di Zheng <dizhengcindy@gmail.com>
2020-02-24 04:55:17 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -75,7 +75,7 @@ def apply_clearance(cart)
   while index < cart.length do
     
   if cart[index][:clearance] 
-    cart[index][:price] *= 0.8
+    cart[index][:price] = (cart[index][:price] * 0.8).round(2)
   end
   index += 1 
   end 
